ba17cf262ddf5f9c1a5a9aea8d8c1a176cbca69d
Ashraf Sayed <ashraf.neizk@gmail.com>
2022-07-25 19:56:07 +0200

    'performance'

--- js/main.js
+++ js/main.js
@@ -25,18 +25,13 @@ document.querySelector(".date").innerHTML = d.getFullYear();
 wordsGroup.addEventListener("change", function (e) {
     chosenLevel = wordsGroup.value;
     words.splice(0,words.length);
-    if(chosenLevel == "champions"){
-        words.push("Aatrox","Ahri","Akali","Akshan","Alistar","Amumu","Anivia","Annie","Aphelios","Ashe","Aurelion Sol","Azir","Bard","Bel'Veth","Blitzcrank","Brand","Braum","Caitlyn","Camille","Cassiopeia","Cho'Gath","Corki","Darius","Diana","Dr.Mundo","Draven","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Gwen","Hecarim","Heimerdinger","Illaoi","Irelia","Ivern","Janna","Jarvan IV","Jax","Jayce","Jhin","Jinx","Kai'Sa","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kayn","Kennen","Kha'Zix","Kindred","Kled","Kog'Maw","LeBlanc","Lee Sin","Leona","Lillia","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","Master Yi","Miss Fortune","Mordekaiser","Morgana","Nami","Nasus","Nautilus","Neeko","Nidalee","Nocturne","Nunu and Willump","Olaf","Orianna","Ornn","Pantheon","Poppy","Pyke","Qiyana","Quinn","Rakan","Rammus","Rek'Sai","Rell","Renata Glasc","Renekton","Rengar","Riven","Rumble","Ryze","Samira","Sejuani","Senna","Seraphine","Sett","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Sylas","Syndra","Tahm Kench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","Twisted Fate","Twitch","Udyr","Urgot","Varus","Vayne","Veigar","Vel'Koz","Vex","VI","Viego","Viktor","Vladimir","Volibear","Warwick","Wukong","Xayah","Xerath","Xin Zhao","Yasuo","Yone","Yorick","Yuumi","Zac","Zed","Zeri","Ziggs","Zilean","Zoe","Zyra")
-    }else if(chosenLevel == "starter-items"){
-        words.push("Cull","Dark Seal","Doran's Blade","Doran's Ring","Doran's Shield","Emberknife","Guardian's Blade","Guardian's Hammer","Guardian's Horn","Guardian's Orb","Hailblade","Relic Shield","Spectral Sickle","Spellthief's Edge","Steel Shoulderguards","Tear of the Goddess","Control Ward","Corrupting Potion","Elixir of Iron","Elixir of Sorcery","Elixir of Wrath","Health Potion","Refillable Potion","Farsight Alteration","Oracle Lens","Stealth Ward","Berserker's Greaves","Boots","Boots of Swiftness","Ionian Boots of Lucidity","Mercury's Treads","Mobility Boots","Plated Steelcaps","Sorcerer's Shoes");
-    }else if(chosenLevel == "basic-items"){
-        words.push("Amplifying Tome","B.F.Sword","Blasting Wand","Stopwatch","Cloak of Agility","Cloth Armor","Dagger","Faerie Charm","Long Sword","Needlessly Large Rod","Null-Magic Mantle","Pickaxe","Rejuvenation Bead","Ruby Crystal","Sapphire Crystal","Sheen")
-    }else if(chosenLevel == "epic-items"){
-        words.push("Aegis of the Legion","Aether Wisp","Bami's Cinder","Bandleglass Mirror","Blighting Jewel","Bramble Vest","Caulfield's Warhammer","Chain Vest","Crystalline Bracer","Executioner's Calling","Fiendish Codex","Forbidden Idol","Frostfang","Giant's Belt","Glacial Buckler","Harrowing Crescent","Hearthbound Axe","Hexdrinker","Hextech Alternator","Ironspike Whip","Kindlegem","Kircheis Shard","Last Whisper","Leeching Leer","Lost Chapter","Negatron Cloak","Noonquiver","Oblivion Orb","Phage","Quicksilver Sash","Rageknife","Recurve Bow","Runesteel Spaulders","Seeker's Armguard","Serrated Dirk","Spectre's Cowl","Targon's Buckler","Tiamat","Vampiric Scepter","Verdant Barrier","Warden's Mail","Watchful Wardstone","Winged Moonplate","Zeal")
-    }else if(chosenLevel == "legendary-items"){
-        words.push("Abyssal Mask","Anathema's Chains","Archangel's Staff","Ardent Censer","Axiom Arc","Banshee's Veil","Black Cleaver","Black Mist Scythe","Blade of the Ruined King","Bloodthirster","Bulwark of the Mountain","Chempunk Chainsword","Chemtech Putrifier","Cosmic Drive","Dead Man's Plate","Death's Dance","Demonic Embrace","Edge of Night","Essence Reaver","Fimbulwinter","Force of Nature","Frozen Heart","Gargoyle Stoneplate","Guardian Angel","Guinsoo's Rageblade","Horizon Focus","Hullbreaker","Infinity Edge","Knight's Vow","Lich Bane","Lord Dominik's Regards","Manamune","Maw of Malmortius","Mejai's Soulstealer","Mercurial Scimitar","Mikael's Blessing","Morellonomicon","Mortal Reminder","Muramana","Nashor's Tooth","Navori QuickBlades","Pauldrons of Whiterock","Phantom Dancer","Rabadon's Deathcap","Randuin's Omen","Rapid Firecannon","Ravenous Hydra","Redemption","Runaan's Hurricane","Rylai's Crystal Scepter","Seraph's Embrace","Serpent's Fang","Serylda's Grudge","Shadowflame","Shard of True Ice","Silvermere Dawn","Spirit Visage","Staff of Flowing Water","Sterak's Gage","Stormrazor","The Collector","Thornmail","Titanic Hydra","Umbral Glaive","Vigilant Wardstone","Void Staff","Warmog's Armor","Winter's Approach","Wit's End","Youmuu's Ghostblade","Zeke's Convergence","Zhonya's Hourglass")
-    }else if(chosenLevel == "mythic-items"){
-        words.push("Crown of the Shattered Queen","Divine Sunderer","Duskblade of Draktharr","Eclipse","Evenshroud","Everfrost","Frostfire Gauntlet","Galeforce","Goredrinker","Hextech Rocketbelt","Immortal Shieldbow","Imperial Mandate","Kraken Slayer","Liandry's Anguish","Locket of the Iron Solari","Luden's Tempest","Moonstone Renewer","Night Harvester","Prowler's Claw","Riftmaker","Shurelya's Battlesong","Stridebreaker","Sunfire Aegis","Trinity Force","Turbo Chemtank")
+    switch(chosenLevel){
+        case "champions":words.push("Aatrox","Ahri","Akali","Akshan","Alistar","Amumu","Anivia","Annie","Aphelios","Ashe","Aurelion Sol","Azir","Bard","Bel'Veth","Blitzcrank","Brand","Braum","Caitlyn","Camille","Cassiopeia","Cho'Gath","Corki","Darius","Diana","Dr.Mundo","Draven","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Gwen","Hecarim","Heimerdinger","Illaoi","Irelia","Ivern","Janna","Jarvan IV","Jax","Jayce","Jhin","Jinx","Kai'Sa","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kayn","Kennen","Kha'Zix","Kindred","Kled","Kog'Maw","LeBlanc","Lee Sin","Leona","Lillia","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","Master Yi","Miss Fortune","Mordekaiser","Morgana","Nami","Nasus","Nautilus","Neeko","Nidalee","Nocturne","Nunu and Willump","Olaf","Orianna","Ornn","Pantheon","Poppy","Pyke","Qiyana","Quinn","Rakan","Rammus","Rek'Sai","Rell","Renata Glasc","Renekton","Rengar","Riven","Rumble","Ryze","Samira","Sejuani","Senna","Seraphine","Sett","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Sylas","Syndra","Tahm Kench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","Twisted Fate","Twitch","Udyr","Urgot","Varus","Vayne","Veigar","Vel'Koz","Vex","VI","Viego","Viktor","Vladimir","Volibear","Warwick","Wukong","Xayah","Xerath","Xin Zhao","Yasuo","Yone","Yorick","Yuumi","Zac","Zed","Zeri","Ziggs","Zilean","Zoe","Zyra"); break;
+        case "starter-items":words.push("Cull","Dark Seal","Doran's Blade","Doran's Ring","Doran's Shield","Emberknife","Guardian's Blade","Guardian's Hammer","Guardian's Horn","Guardian's Orb","Hailblade","Relic Shield","Spectral Sickle","Spellthief's Edge","Steel Shoulderguards","Tear of the Goddess","Control Ward","Corrupting Potion","Elixir of Iron","Elixir of Sorcery","Elixir of Wrath","Health Potion","Refillable Potion","Farsight Alteration","Oracle Lens","Stealth Ward","Berserker's Greaves","Boots","Boots of Swiftness","Ionian Boots of Lucidity","Mercury's Treads","Mobility Boots","Plated Steelcaps","Sorcerer's Shoes");; break;
+        case "basic-items":words.push("Amplifying Tome","B.F.Sword","Blasting Wand","Stopwatch","Cloak of Agility","Cloth Armor","Dagger","Faerie Charm","Long Sword","Needlessly Large Rod","Null-Magic Mantle","Pickaxe","Rejuvenation Bead","Ruby Crystal","Sapphire Crystal","Sheen"); break;
+        case "epic-items":words.push("Aegis of the Legion","Aether Wisp","Bami's Cinder","Bandleglass Mirror","Blighting Jewel","Bramble Vest","Caulfield's Warhammer","Chain Vest","Crystalline Bracer","Executioner's Calling","Fiendish Codex","Forbidden Idol","Frostfang","Giant's Belt","Glacial Buckler","Harrowing Crescent","Hearthbound Axe","Hexdrinker","Hextech Alternator","Ironspike Whip","Kindlegem","Kircheis Shard","Last Whisper","Leeching Leer","Lost Chapter","Negatron Cloak","Noonquiver","Oblivion Orb","Phage","Quicksilver Sash","Rageknife","Recurve Bow","Runesteel Spaulders","Seeker's Armguard","Serrated Dirk","Spectre's Cowl","Targon's Buckler","Tiamat","Vampiric Scepter","Verdant Barrier","Warden's Mail","Watchful Wardstone","Winged Moonplate","Zeal"); break;
+        case "legendary-items":words.push("Abyssal Mask","Anathema's Chains","Archangel's Staff","Ardent Censer","Axiom Arc","Banshee's Veil","Black Cleaver","Black Mist Scythe","Blade of the Ruined King","Bloodthirster","Bulwark of the Mountain","Chempunk Chainsword","Chemtech Putrifier","Cosmic Drive","Dead Man's Plate","Death's Dance","Demonic Embrace","Edge of Night","Essence Reaver","Fimbulwinter","Force of Nature","Frozen Heart","Gargoyle Stoneplate","Guardian Angel","Guinsoo's Rageblade","Horizon Focus","Hullbreaker","Infinity Edge","Knight's Vow","Lich Bane","Lord Dominik's Regards","Manamune","Maw of Malmortius","Mejai's Soulstealer","Mercurial Scimitar","Mikael's Blessing","Morellonomicon","Mortal Reminder","Muramana","Nashor's Tooth","Navori QuickBlades","Pauldrons of Whiterock","Phantom Dancer","Rabadon's Deathcap","Randuin's Omen","Rapid Firecannon","Ravenous Hydra","Redemption","Runaan's Hurricane","Rylai's Crystal Scepter","Seraph's Embrace","Serpent's Fang","Serylda's Grudge","Shadowflame","Shard of True Ice","Silvermere Dawn","Spirit Visage","Staff of Flowing Water","Sterak's Gage","Stormrazor","The Collector","Thornmail","Titanic Hydra","Umbral Glaive","Vigilant Wardstone","Void Staff","Warmog's Armor","Winter's Approach","Wit's End","Youmuu's Ghostblade","Zeke's Convergence","Zhonya's Hourglass"); break;
+        case "mythic-items":words.push("Crown of the Shattered Queen","Divine Sunderer","Duskblade of Draktharr","Eclipse","Evenshroud","Everfrost","Frostfire Gauntlet","Galeforce","Goredrinker","Hextech Rocketbelt","Immortal Shieldbow","Imperial Mandate","Kraken Slayer","Liandry's Anguish","Locket of the Iron Solari","Luden's Tempest","Moonstone Renewer","Night Harvester","Prowler's Claw","Riftmaker","Shurelya's Battlesong","Stridebreaker","Sunfire Aegis","Trinity Force","Turbo Chemtank"); break;
     }
     scoreTotal.innerHTML = words.length;
 })
@@ -58,7 +53,6 @@ levelSelector.addEventListener("change", (e) => {
 input.onpaste = function() {
     return false;
 }
-
 // Start Game
 startButton.onclick = function() {
     scoreGot.innerHTML = 0;
@@ -75,7 +69,6 @@ startButton.onclick = function() {
     input.focus();
     genWords();
 }
-
 function genWords() {
     if (levelName == "Hard") {
         theWord.style.display = "none";
@@ -110,12 +103,12 @@ function startPlay() {
                     genWords();
                 } else {
                     span.className = 'good';
-                    span.innerHTML = `Conrgatulation! you achived ${(scoreGot.innerHTML * 100 / scoreTotal.innerHTML).toFixed(2)}% on ${levelName} difficulty ${wordsGroup.value} Group`;
+                    span.innerHTML = `Conrgatulation!🥳 you achived ${(scoreGot.innerHTML * 100 / scoreTotal.innerHTML).toFixed(2)}% on ${levelName} difficulty ${wordsGroup.value} Group`;
                     upcomingWords.remove();
                 }
             } else {
                 span.className = 'bad';
-                span.innerHTML = `Game Over! you achived ${(scoreGot.innerHTML * 100 / scoreTotal.innerHTML).toFixed(2)}% on ${levelName} difficulty ${wordsGroup.value} Group`;
+                span.innerHTML = `Game Over!🥺 you achived ${(scoreGot.innerHTML * 100 / scoreTotal.innerHTML).toFixed(2)}% on ${levelName} difficulty ${wordsGroup.value.toUpperCase()} Group`;
             }
             clearInterval(start);
         }
@@ -123,4 +116,3 @@ function startPlay() {
         levelSeconds--
     }, 1000);
 }
-m
